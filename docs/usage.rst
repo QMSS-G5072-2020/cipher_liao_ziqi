ce1c1faf5669b2c2f52fccbd0ffd2101783a3f7a
Ziqi Liao <zl2988@columbia.edu>
2020-11-14 16:48:39 +0800

    Create directory structure

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use cipher_liao_ziqi in a project::
+
+    import cipher_liao_ziqi
---
+++ tests/test_cipher_liao_ziqi.py
@@ -0,0 +1,5 @@
+from cipher_liao_ziqi import __version__
+from cipher_liao_ziqi import cipher_liao_ziqi
+
+def test_version():
+    assert __version__ == '0.1.0'
